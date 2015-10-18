ce3a1c8de45611ff52046ea1b464774ad71dda7b
Josh Taylor <joshrtay@gmail.com>
2015-10-18 16:25:11 -0700

    FIRST

---
+++ src/index.js
@@ -0,0 +1,114 @@
+/**
+ * Imports
+ */
+import is from '@weo-edu/is'
+
+/**
+ * key-diff
+ */
+
+function diff (prev, next, effect) {
+  let pStart = cursor(prev, 0)
+  let pEnd = cursor(prev, prev.length - 1)
+
+  let nStart = cursor(next, 0)
+  let nEnd = cursor(next, next.length - 1)
+
+  let keyToIdx, idxInPrev, before;
+
+  while (pStart.idx <= pEnd.idx && nStart.idx <= nEnd.idx) {
+    if (is.undefined(pStart.item)) {
+      pStart = forward(prev, pStart)
+    } else if (is.undefined(pEnd.item)){
+      pEnd = back(prev, pEnd)
+    } else if (equal(pStart, nStart)){
+      effect(change('DIFF_UPDATE', pStart, nStart))
+      pStart = forward(prev, pStart)
+      nStart = forward(next, nStart)
+    } else if (equal(pEnd, nEnd)) {
+      effect(change('DIFF_UPDATE', pEnd, nEnd))
+      pEnd = back(prev, pEnd)
+      nEnd = back(next, nEnd)
+    } else if (equal(pStart, nEnd)) {
+      effect(change('DIFF_MOVE', pStart, nEnd, pEnd.idx))
+      pStart = forward(prev, pStart)
+      nEnd = back(next, nEnd)
+    } else if (equal(pEnd, nStart)) {
+      effect(change('DIFF_MOVE', pEnd, nStart, pStart.idx))
+      pEnd = back(prev, pEnd)
+      nStart = forward(next, nStart)
+    } else {
+      if (is.undefined(keyToIdx)) {
+        keyToIdx = mapKeyToIdx(prev, prev.idx, pEnd.idx)
+      }
+      idxInPrev = keyToIdx[key(nStart)]
+      if (is.undefined(idxInPrev)) {
+        effect(change('DIFF_CREATE', null, nStart, pStart.idx))
+      } else {
+        effect(change('DIFF_MOVE', prev[idxInPrev], nStart, pStart.idx))
+        delete keyToIdx[key(nStart)]
+      }
+      nStart = forward(next, nStart)
+    }
+  }
+
+  if (pStart.idx > pEnd.idx) {
+    for (; nStart.idx <= nEnd.idx; nStart = forward(next, nStart)) {
+        effect(change('DIFF_CREATE', null, nStart, nEnd.idx))
+    }
+  } else if (nStart.idx > nEnd.idx) {
+    for(; pStart.idx <= pEnd.idx; pStart = forward(prev, pStart)) {
+      effect(change('DIFF_REMOVE', pStart))
+    }
+  }
+
+}
+
+function cursor (list, idx) {
+  return {
+    item: list[idx],
+    idx: idx
+  }
+}
+
+function forward (list, c) {
+  return cursor(list, c.idx + 1)
+}
+
+function back (list, c) {
+  return cursor(list, c.idx - 1)
+}
+
+function change (type, prev, next, pos) {
+  let action = {type, prev, next}
+  if (!is.undefined(pos)) {
+    action.pos = pos
+  }
+  return action
+}
+
+function equal(prev, next) {
+  return key(prev) === key(next)
+}
+
+function key(cursor) {
+  return cursor.item.key || cursor.idx
+}
+
+function mapKeyToIdx(list, start, end) {
+  let i
+  let key
+  let map = {}
+
+  for (i = start; i <= end; ++i) {
+    key = list[i].key;
+    if (!is.undefined(key)) map[key] = i;
+  }
+  return map;
+}
+
+/**
+ * Exports
+ */
+
+export default diff
---
+++ test/index.js
@@ -0,0 +1,249 @@
+/**
+ * Imports
+ */
+
+import test from 'tape'
+import diff from '../src'
+
+/**
+ * Tests
+ */
+
+test('add', (t) => {
+  let a = []
+  let b = [{key: 'foo', val: 'bar'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('add many', (t) => {
+  let a = []
+  let b = [{key: 'foo', val: 'bar'}, {key: 'bat', val: 'box'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('add before/after', (t) => {
+  let a = [{key: 'bar', val: 'two'}]
+  let b = [
+    {key: 'foo', val: 'one'},
+    {key: 'bar', val: 'two'},
+    {key: 'baz', val: 'three'}
+  ]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('add middle', (t) => {
+  let a = [{key: 'foo', val: 'one'}, {key: 'baz', val: 'four'}]
+  let b = [
+    {key: 'foo', val: 'one'},
+    {key: 'bar', val: 'five'},
+    {key: 'baz', val: 'four'}
+  ]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('remove', (t) => {
+  let a = [{key: 'foo', val: 'bar'}]
+  let b = []
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('remove many', (t) => {
+  let a = [{key: 'foo', val: 'bar'}, {key: 'bat', val: 'box'}]
+  let b = []
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('remove one', (t) => {
+  let a = [{key: 'bar', val: 'two'}, {key: 'foo', val: 'one'}]
+  let b = [{key: 'bar', val: 'two'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('update', (t) => {
+  let a = [{key: 'foo', val: 'bar'}]
+  let b = [{key: 'foo', val: 'box'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('update/remove', (t) => {
+  let a = [
+    {key: 'foo', val: 'one'},
+    {key: 'bar', val: 'two'},
+    {key: 'baz', val: 'three'}
+  ]
+  let b = [{key: 'foo', val: 'one'}, {key: 'baz', val: 'four'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('update/remove 2', (t) => {
+  let a = [
+    {key: 'foo', val: 'one'},
+    {key: 'bar', val: 'five'},
+    {key: 'baz', val: 'four'}
+  ]
+  let b = [{key: 'foo', val: 'one'}, {key: 'bar', val: 'span'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('update/remove 3', (t) => {
+  let a = [
+    {key: 'bar', val: 'span'},
+    {key: 'foo', val: 'one'}
+  ]
+  let b = [{key: 'foo', val: 'span'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+test('swap', (t) => {
+  let a = [{key: 'foo', val: 'bar'}, {key: 'bat', val: 'box'}]
+  let b = [{key: 'bat', val: 'box'}, {key: 'foo', val: 'bar'}]
+  let c = clone(a)
+  let patch = update(c)
+
+  diff(a, b, patch)
+
+  t.deepEqual(c, b)
+
+  t.end()
+})
+
+
+
+
+function update(list) {
+  return function(action) {
+    switch(action.type) {
+      case 'DIFF_CREATE':
+        insertAt(list, action.pos, action.next.item)
+        break
+      case 'DIFF_REMOVE':
+        remove(list, action.prev.item)
+        break
+      case 'DIFF_MOVE':
+        patch(list, action.prev.item, action.next.item)
+        move(list, action.pos, action.prev.item)
+        break
+      case 'DIFF_UPDATE':
+        patch(list, action.prev.item, action.next.item)
+        break
+    }
+  }
+}
+
+function insertAt (list, idx, item) {
+  if (list[idx]) {
+    list.splice(idx, 0, item)
+  } else {
+    list.push(item)
+  }
+}
+
+function indexOf (list, item) {
+  let i = 0
+  for (; i < list.length; ++i) {
+    if (list[i] === item) {
+      return i
+    }
+  }
+  return -1
+}
+
+function remove (list, item) {
+  list.splice(indexOf(list, item), 1)
+}
+
+function move(list, idx, item) {
+  remove(list, item)
+  insertAt(list, idx, item)
+}
+
+function patch(list, pItem, nItem) {
+  for (let key in pItem) {
+    delete pItem[key]
+  }
+  for (let key in nItem) {
+    pItem[key] = nItem[key]
+  }
+  return pItem
+}
+
+function clone (list) {
+  return list.slice(0)
+}
